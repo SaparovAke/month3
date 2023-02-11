26de35e286dc34f8366a0085f0c151829d07363e
SaparovAke <saparov.ake@gmail.com>
2023-02-11 18:37:26 +0600

    ''

--- HW2.py
+++ HW2.py
@@ -1,4 +1,4 @@
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, types
 from aiogram.utils import executor
 from decouple import config
 
@@ -8,9 +8,13 @@ bot = Bot(TOKEN)
 db = Dispatcher(bot=bot)
 
 @db.message_handler(commands=['hello', 'salam'])
-async def start_handler(message:types.Message):
+async def start_handler(message: types.Message):
     await bot.send_message(message.from_user.id, f'Hello {message.from_user.first_name}')
+    await message.reply('пока что всё')
 
+@db.message_handler()
+async def echo(message: types.Message):
+    await bot.send_message(message.from_user.id, message.text)
 
 if __name__ == '__main__':
     executor.start_polling(db, skip_updates=True)
