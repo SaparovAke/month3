c41b1c68abc3273e9acadfa9b409c0c405b307c8
SaparovAke <saparov.ake@gmail.com>
2023-02-11 18:21:26 +0600

    'reg'

--- HW2.py
+++ HW2.py
@@ -7,5 +7,10 @@ TOKEN = config('TOKEN')
 bot = Bot(TOKEN)
 db = Dispatcher(bot=bot)
 
+@db.message_handler(commands=['hello', 'salam'])
+async def start_handler(message:types.Message):
+    await bot.send_message(message.from_user.id, f'Hello {message.from_user.first_name}')
+
+
 if __name__ == '__main__':
     executor.start_polling(db, skip_updates=True)
